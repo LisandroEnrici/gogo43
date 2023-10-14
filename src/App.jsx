253d9cc16b9c494eaa3e0f917a78dfa55cc1e9ac
Lisandro <lisandroenrici@gmail.com>
2023-10-14 15:24:42 +0200

    animated banner created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Banner from "./sections/banner/banner";
 
 function App() {
   return (
-    <h1>Hello world</h1>
+  <>
+    <Banner />
+  </>
   )
 }
 
---
+++ src/sections/banner/banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './banner.css';
+
+function Banner() {
+  return (
+    <section id="banner" >
+      <div id="heartbeat-gradient" className="full-size">
+        hola
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
