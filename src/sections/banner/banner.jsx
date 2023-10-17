36984134965d8ec247a027c83acfd94b0f510918
Lisandro <lisandroenrici@gmail.com>
2023-10-17 19:03:36 +0200

    Banner done, missing animation

--- src/sections/banner/banner.jsx
+++ src/sections/banner/banner.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import './banner.css';
+import "./banner.css";
+import logoWhite from "../../assets/logoWhite.png";
+import { BsChevronDoubleDown } from "react-icons/bs";
 
 function Banner() {
   return (
-    <section id="banner" >
-      <div id="heartbeat-gradient" className="full-size">
-        hola
+    <section id="banner">
+      <img
+        id="logo-img"
+        src={logoWhite}
+        alt="Gogo 43 Aci Trezza"
+      />
+      <div id="bottom-section">
+        <h2 className="neon-text">Qui dove battono i quori</h2>
+        <BsChevronDoubleDown id="scroll-icon" className="neon-text"/>
       </div>
     </section>
   );
