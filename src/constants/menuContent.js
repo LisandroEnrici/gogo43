b35dc7e2315a2db28d2a9fba5fbcb9c60c853c0c
Lisandro <lisandroenrici@gmail.com>
2023-11-04 17:29:17 +0100

    menu content created

---
+++ src/constants/menuContent.js
@@ -0,0 +1,674 @@
+export const italianMenu = [
+  {
+    title: "COCKTAILS",
+    imgUrl: "src/assets/food/sour.jpg",
+    content: [
+      {
+        title: "DRINK LIST",
+        icons: [],
+        clarifications: "",
+        content: [
+          {
+            name: "COCKTAIL IBA",
+            icon: "",
+            description: "Garnish by Gogo 43",
+            price: "9",
+          },
+          {
+            name: "PA-MI",
+            icon: "Campari, Amaro tutone, Succo di arancia, Sciroppo di zucchero, Orinoco bitter",
+            description: "",
+            price: "10",
+          },
+          {
+            name: "TROPICUS",
+            icon: "",
+            description:
+              "Italicus, Liquore al melone, Lime, Mango, Bitter pompelmo, Succo di lime",
+            price: "10",
+          },
+          {
+            name: "MR. RYE",
+            icon: "",
+            description: "Bullet rye, Succo di limome, Melagrana, Coca Cola",
+            price: "10",
+          },
+          {
+            name: "MISS BRAMBLE",
+            icon: "",
+            description:
+              "Bacardi bianco, Chambord, Succo di lime, Lamponi, More",
+            price: "11",
+          },
+          {
+            name: "ITALIAN MOTHERFUCKER",
+            icon: "",
+            description:
+              "Italicus, Gin, Vodka, Rum, Sweet&Sour, Ginger beer, Albumina",
+            price: "11",
+          },
+          {
+            name: "HENDRICKS MOJITO",
+            icon: "",
+            description:
+              "Hendricks gin, Cocumber, Fleurs de sureau, Menta, Succo di lime, Zucchero di canna",
+            price: "13",
+          },
+          {
+            name: "WHISKEY SOUR",
+            icon: "",
+            description:
+              "Wild Turkey bourbon, Succo di limone, Sciroppo di zucchero",
+            price: "9",
+          },
+          {
+            name: "OLD FASHIONED",
+            icon: "",
+            description: "Wild Turkey 101, Acqua, Zucchero, Aromatic bitter",
+            price: "9",
+          },
+          {
+            name: "AMERICANO",
+            icon: "",
+            description: "Campari, Cinzano vermouth rosso, Seltz",
+            price: "9",
+          },
+          {
+            name: "NEGRONI",
+            icon: "",
+            description: "Campari, Cinzano vermouth rosso, Bulldog gin",
+            price: "9",
+          },
+          {
+            name: "PALOMA",
+            icon: "",
+            description:
+              "Tequila Espolon Blanco, Succo di lime, Sciroppo di agave, Thomas Henry pink grapefruit",
+            price: "9",
+          },
+          {
+            name: "CAMPARI SPRITZ",
+            icon: "",
+            description: "Campari, Prosecco Valdo, Seltz",
+            price: "",
+          },
+        ],
+      },
+      {
+        title: "ANALCOLICI",
+        icons: [],
+        clarifications: "",
+        content: [
+          {
+            name: "VIRGIN PASSION",
+            icon: "",
+            description: "Premix passion fruit, Sweet&Sour, Succo alla pesca",
+            price: "6",
+          },
+          {
+            name: "VIRGIN COLADA",
+            icon: "",
+            description: "Premix cocco, Succo all’ananas",
+            price: "6",
+          },
+          {
+            name: "VIRGIN MOJITO",
+            icon: "",
+            description: "Lime pestato, Menta, Zucchero di canna, Lemon soda",
+            price: "6",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "VINI",
+    imgUrl: "src/assets/food/piattoRodo.jpg",
+    content: [
+      {
+        title: "VINI ROSSI",
+        icons: [],
+        clarifications: "",
+        content: [
+          {
+            name: "NERO D'AVOLA",
+            icon: "",
+            description: "Cantina: Baglio di Pianetto",
+            price: "6 / 24",
+          },
+          {
+            name: "SIRAH",
+            icon: "",
+            description: "Cantina: Baglio di Pianetto",
+            price: "6 / 24",
+          },
+          {
+            name: "FRAPPATO",
+            icon: "",
+            description: "Cantina: Baglio di Pianetto",
+            price: "6 / 24",
+          },
+          {
+            name: "ETNA ROSSO",
+            icon: "",
+            description: "Cantina: Iupa",
+            price: "- / 24",
+          },
+          {
+            name: "ROSE",
+            icon: "",
+            description: "Cantina: Iupa",
+            price: "- / 24",
+          },
+          {
+            name: "ETNA ROSSO",
+            icon: "",
+            description: "Cantina: Girolamo Russo",
+            price: "- / 38",
+          },
+        ],
+      },
+      {
+        title: "VINI BIANCHI",
+        icons: [],
+        clarifications: "",
+        content: [
+          {
+            name: "INZOLIA CHARDONNAY",
+            icon: "",
+            description: "Cantina: Canicatti",
+            price: "6 / 24",
+          },
+          {
+            name: "ETNA BIANCO",
+            icon: "",
+            description: "Cantina: Iupa",
+            price: "6 / 24",
+          },
+          {
+            name: "MOSCATO BIANCO",
+            icon: "",
+            description: "Cantina: Planeta",
+            price: "6 / 24",
+          },
+          {
+            name: "GRILLO",
+            icon: "",
+            description: "Cantina: Judeka",
+            price: "6 / 24",
+          },
+          {
+            name: "CUCUNCI",
+            icon: "",
+            description: "Cantina: Montalto",
+            price: "6 / 24",
+          },
+          {
+            name: "FRABIANCO",
+            icon: "",
+            description: "Cantina: Judeka",
+            price: "6 / 24",
+          },
+        ],
+      },
+      {
+        title: "PROSECCO",
+        icons: [],
+        clarifications: "",
+        content: [
+          {
+            name: "VALDO",
+            icon: "",
+            description: "",
+            price: "6 / 28",
+          },
+        ],
+      },
+      {
+        title: "FRANCIACORTA",
+        icons: [],
+        clarifications: "",
+        content: [
+          {
+            name: "BERLUCCHI SATEN",
+            icon: "",
+            description: "",
+            price: "37",
+          },
+          {
+            name: "CÀ DEL BOSCO",
+            icon: "",
+            description: "",
+            price: "52",
+          },
+          {
+            name: "BELLAVISTA",
+            icon: "",
+            description: "",
+            price: "55",
+          },
+        ],
+      },
+      {
+        title: "CHAMPAGNE",
+        icons: [],
+        clarifications: "",
+        content: [
+          {
+            name: "CRISTAL",
+            icon: "",
+            description: "",
+            price: "380",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "BIRRE",
+    imgUrl: "",
+    content: [
+      {
+        title: "BIRRE",
+        icons: [],
+        clarifications: "",
+        content: [
+          {
+            name: "BITBURGER 500CL",
+            icon: "",
+            description: "",
+            price: "5",
+          },
+          {
+            name: "ERDINGER 500CL",
+            icon: "",
+            description: "",
+            price: "6",
+          },
+          {
+            name: "FREAK IPA",
+            icon: "",
+            description: "",
+            price: "6",
+          },
+          {
+            name: "CORONA",
+            icon: "",
+            description: "",
+            price: "5",
+          },
+          {
+            name: "BUDWEISER",
+            icon: "",
+            description: "",
+            price: "4",
+          },
+          {
+            name: "BIRRA DELLO STRETTO",
+            icon: "",
+            description: "",
+            price: "5",
+          },
+          {
+            name: "AYINGER CELEBRATOR",
+            icon: "",
+            description: "",
+            price: "6",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "DISTILATI",
+    imgUrl: "src/assets/food/whiskey.jpg",
+    content: [
+      {
+        title: "DISTILLATI E LIQUORI",
+        icons: [],
+        clarifications: "Il personale è ritenuto a conferire i prezzi.",
+        content: [
+          {
+            name: "SHOT",
+            icon: "",
+            description: "",
+            price: "Da 4 a 45",
+          },
+          {
+            name: "GIN",
+            icon: "",
+            description: "",
+            price: "Da 6 a 90",
+          },
+          {
+            name: "VODKA",
+            icon: "",
+            description: "",
+            price: "Da 6 a 90",
+          },
+          {
+            name: "RUM",
+            icon: "",
+            description: "",
+            price: "Da 6 a 90",
+          },
+          {
+            name: "VERMOUTH",
+            icon: "",
+            description: "",
+            price: "Da 6 a 90",
+          },
+          {
+            name: "WHISKEY / WHISKY",
+            icon: "",
+            description: "",
+            price: "Da 6 a 90",
+          },
+          {
+            name: "LIQUORI",
+            icon: "",
+            description: "",
+            price: "Da 6 a 90",
+          },
+          {
+            name: "TEQUILA",
+            icon: "",
+            description: "",
+            price: "Da 6 a 90",
+          },
+          {
+            name: "AMARI",
+            icon: "",
+            description: "",
+            price: "Da 5 a 8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "BEBANDE",
+    imgUrl: "",
+    content: [
+      {
+        title: "BEVANDE",
+        icons: [],
+        clarifications: "",
+        content: [
+          {
+            name: "TONICHE / BIBITE",
+            icon: "",
+            description: "",
+            price: "3 / 5",
+          },
+          {
+            name: "ACQUA PANNA",
+            icon: "",
+            description: "",
+            price: "2,5",
+          },
+          {
+            name: "ACQUA FRIZZANTE SAN PELLEGRINO",
+            icon: "",
+            description: "",
+            price: "2,5",
+          },
+          {
+            name: "ACQUA LETE",
+            icon: "",
+            description: "",
+            price: "2,5",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "ANTIPASTI",
+    imgUrl: "src/assets/food/piattoDeg.jpg",
+    content: [
+      {
+        title: "SFIZIOSITÁ",
+        icons: [],
+        clarifications:
+          "I prodotti contrassegnati con *, in alcuni periodi dell’anno potrebbero essere surgelati.",
+        content: [
+          {
+            name: "STICK DI MOZZARELLA *",
+            icon: "",
+            description: "Mozzarella stick cheese",
+            price: "6,5",
+          },
+          {
+            name: "STRACCETTI DI POLLO *",
+            icon: "",
+            description: "Chicken stick",
+            price: "6,5",
+          },
+          {
+            name: "ALI E COSCE DI POLLO *",
+            icon: "",
+            description: "Chicken wings and legs",
+            price: "8",
+          },
+          {
+            name: "MOZZARELLINE AL TARTUFO *",
+            icon: "",
+            description: "Mozzarelline with truffle",
+            price: "6,5",
+          },
+          {
+            name: "FIORI DI ZUCCA *",
+            icon: "",
+            description: "Courgette flowers",
+            price: "4",
+          },
+          {
+            name: "PATATINE FRITTE *",
+            icon: "",
+            description: "Chips",
+            price: "4,5",
+          },
+          {
+            name: "GAMBERETTI FRITTI",
+            icon: "",
+            description: "Fry shrimps",
+            price: "6",
+          },
+        ],
+      },
+      {
+        title: "BRUSCACCIE",
+        icons: [],
+        clarifications: "Half bread-dipping, Half bun",
+        content: [
+          {
+            name: "OLIO, ORIGANO, SALE, PEPE",
+            icon: "",
+            description: "Oil, Oregano, Salt, Pepper",
+            price: "2",
+          },
+          {
+            name: "POMODORO, OLIO, ORIGANO, BASILICO",
+            icon: "",
+            description: "Tomatoes, Oil, Oregano, Basil",
+            price: "5",
+          },
+          {
+            name: "TONNO, CAPPERI, CIPOLLA, POMODORO, MOZZARELLA",
+            icon: "",
+            description: "Tuna, Capers, Onion, Tomato, Mozzarella cheese",
+            price: "9",
+          },
+          {
+            name: "SALSICCIA, FRIARIELLI, MOZZARELLA E SCAGLIE DI PEPATO",
+            icon: "",
+            description:
+              "Sausage, friarielli, mozzarella cheese and pepato cheese",
+            price: "11",
+          },
+          {
+            name: "BURRATA, GAMBERO ROSSO DI MAZARA, PISTACHIO",
+            icon: "",
+            description: "Burrata (Creamy cheese), Red shrimps, pistachio",
+            price: "6",
+          },
+        ],
+      },
+      {
+        title: "ANTIPASTI",
+        icons: [],
+        clarifications: "Pre dinner",
+        content: [
+          {
+            name: "PEPATA DI COZZE",
+            icon: "",
+            description: "Mussels cooked with pepper and parsley",
+            price: "11",
+          },
+          {
+            name: "CREMA DI PARMIGIANO CON UOVOE TARTUFO INTARSIATO ESTIVO",
+            icon: "",
+            description: "Parmesan cream witg egg and sunmer inlaid truffle",
+            price: "9",
+          },
+          {
+            name: "INSALATA",
+            icon: "",
+            description: "Salad",
+            price: "4 / 9",
+          },
+          {
+            name: "PIATTO DEGUSTAZIONE",
+            icon: "",
+            description: "Tasting dish",
+            price: "19",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "PRIMI",
+    imgUrl: "src/assets/food/piattoRodo.jpg",
+    content: [
+      {
+        title: "PRIMI PIATTI",
+        icons: [],
+        clarification: "First dishes",
+        content: [
+          {
+            name: "LINGUINE PISTACCHIO E GAMBERI *",
+            icon: "",
+            description: "Linguine with shrimps* and pistachio",
+            price: "16",
+          },
+          {
+            name: "PIATTO STELLA",
+            icon: "",
+            description: "Linguine with lobster* 300g and tomatoes",
+            price: "23",
+          },
+          {
+            name: "SPAGHETTI BOTTARGA DI TONNO",
+            icon: "",
+            description: "Spaghetti with bottarga of tuna, lime",
+            price: "17",
+          },
+          {
+            name: "FIDATI DI NOI",
+            icon: "",
+            description: "Trust us",
+            price: "22",
+          },
+          {
+            name: "PIATTO DEL GIORNO",
+            icon: "",
+            description: "Daily special",
+            price: "17 / 23",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "SECONDI",
+    imgUrl: "src/assets/food/calamaroRip.jpg",
+    content: [
+      {
+        title: "SECONDI PIATTI",
+        icons: [],
+        clarification: "Second dishes",
+        content: [
+          {
+            name: "SCOPPIATA DI MARE",
+            icon: "",
+            description: "Aragosta*, Gamberi*, Calamaro*, polipo* e cozze",
+            price: "35",
+          },
+          {
+            name: "STINCO DI MAIALE CON CREMA AL PARMIGIANO",
+            icon: "",
+            description: "Pork shank with parmigiano cheese cream",
+            price: "18",
+          },
+          {
+            name: "COSCIA D’ANATRA CON CREMA AL PARMIGIANO",
+            icon: "",
+            description: "Thigh of duck with parmigiano cheese cream",
+            price: "18",
+          },
+          {
+            name: "CALAMARO FRITTO E CHIPS",
+            icon: "",
+            description: "Fried Squid and Chips",
+            price: "18",
+          },
+          {
+            name: "CALAMARO RIPIENO",
+            icon: "",
+            description: "Stuffed squid",
+            price: "16",
+          },
+          {
+            name: "SECONDO DEL GIORNO",
+            icon: "",
+            description: "Second daily special",
+            price: "16 / 22",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "DOLCI",
+    imgUrl: "",
+    content: [
+      {
+        title: "DOLCI",
+        icons: [],
+        clarification: "Desserts",
+        content: [
+          {
+            name: "NEW YORK CHEESCAKE CARAMELLO",
+            icon: "",
+            description: "New York caramel cheescake",
+            price: "5",
+          },
+          {
+            name: "NEW YORK CHEESCAKE CIOCCOLATO",
+            icon: "",
+            description: "New York chocolate cheescake",
+            price: "5",
+          },
+          {
+            name: "NEW YORK CHEESCAKE FRUTTI DI BOSCO",
+            icon: "",
+            description: "New York wild berries cheescake",
+            price: "5",
+          },
+        ],
+      },
+    ],
+  },
+];
