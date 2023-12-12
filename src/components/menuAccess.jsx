796b16f8967b641b7c42255f814414358d7b972b
Lisandro <lisandroenrici@gmail.com>
2023-12-12 20:05:59 +0100

    cosmetic bugs on mobile devices fixed up

--- src/components/menuAccess.jsx
+++ src/components/menuAccess.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function MenuAccess() {
   return (
-    <section>
+    <section id="menu-access">
       <div className="menu-access-container">
         <div>
           <h1>ME</h1>
