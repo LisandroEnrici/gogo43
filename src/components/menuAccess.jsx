9adebe595f82fd075b6c08dc0aba0b04df28f4a2
Lisandro <lisandroenrici@gmail.com>
2025-08-11 08:35:56 -0300

    menuAccess link hierarchy changed

--- src/components/menuAccess.jsx
+++ src/components/menuAccess.jsx
@@ -5,15 +5,13 @@ import { Link } from "react-router-dom";
 function MenuAccess() {
   return (
     <section id="menu-access">
-      <div className="menu-access-container">
+      <Link to="menu" className="menu-access-container">
         <div>
           <h1>ME</h1>
           <h1 style={{ letterSpacing: "10px" }}>NU</h1>
         </div>
-        <Link to="menu">
-          <BsChevronDoubleRight className="arrow-icon" />
-        </Link>
-      </div>
+        <BsChevronDoubleRight className="arrow-icon" />
+      </Link>
     </section>
   );
 }
